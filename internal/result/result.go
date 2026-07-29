a4035dc35a3fce625fbab12f87375c746304f1eb
3rr0r-505 <samratdey.career+dev@gmail.com>
2026-07-29 10:55:06 +0530

    feat(result): add PageResult struct with constructor

--- internal/result/result.go
+++ internal/result/result.go
@@ -1,1 +1,19 @@
 package result
+
+import "time"
+
+type PageResult struct {
+	Url       string
+	Status    int
+	Depth     int
+	TimeStamp time.Time
+	Error     string
+}
+
+func NewPage(url string, depth int) PageResult {
+	return PageResult{
+		Url:       url,
+		Depth:     depth,
+		TimeStamp: time.Now(),
+	}
+}
