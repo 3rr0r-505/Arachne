6ead60ce85e4d19d7e5d40f0cda3f7368510c207
3rr0r-505 <samratdey.career+dev@gmail.com>
2026-07-29 20:40:26 +0530

    feat(crawler): add JobQ buffered channel wrapper
    - Job struct: URL + depth pair
    - Push/Pop wrap channel send/receive
    - Pop preserves ok flag for future close-detection in orchestrator

--- internal/crawler/queue.go
+++ internal/crawler/queue.go
@@ -1,1 +1,25 @@
 package crawler
+
+type Job struct {
+	URL   string
+	Depth int
+}
+
+type JobQ struct {
+	jobs chan Job
+}
+
+func NewJobQueue(size int) *JobQ {
+	return &JobQ{
+		jobs: make(chan Job, size),
+	}
+}
+
+func (jq *JobQ) Push(job Job) {
+	jq.jobs <- job
+}
+
+func (jq *JobQ) Pop() (Job, bool) {
+	job, ok := <-jq.jobs
+	return job, ok
+}
